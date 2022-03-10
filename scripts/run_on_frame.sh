1a196d419cf685d93e6987bfdb2fa51ec1acdbf6
Simon Perriard <simon.perriard@chainsecurity.com>
2022-03-10 08:40:15 +0100

    typo

--- scripts/run_on_frame.sh
+++ scripts/run_on_frame.sh
@@ -5,7 +5,7 @@ git clone git@github.com:paritytech/substrate.git
 cd substrate/frame/
 
 no_extract=("benchmarking" "examples" "election-provider-support" "executive" "staking" "try-runtime" "whitelist")
-# ne_extract contains folders that are not pallets
+# no_extract contains folders that are not pallets
 # whitelist is a pallet but does not even build on Substrate repo
 
 for pallet in *
