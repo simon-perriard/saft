b6a4b6f195fdc5078125ede7bc733c43931748cf
Simon Perriard <simon.perriard@epfl.ch>
2022-04-07 21:48:04 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     env,
     path::PathBuf,
-    process::{self, Command},
+    process::{self, Command}, ffi::OsString,
 };
 
 struct CargoSaftCmd {
@@ -28,13 +28,10 @@ impl CargoSaftCmd {
         // Every remaining arg is rustc arg
         args.append(&mut (old_args.collect()));
 
-        Self {
-            cargo_subcmd,
-            args,
-        }
+        Self { cargo_subcmd, args }
     }
 
-    fn analyzer_path() -> PathBuf {
+    fn driver_path() -> PathBuf {
         let mut path = env::current_exe()
             .expect("invalid executable path")
             .with_file_name("saft-driver");
@@ -49,9 +46,33 @@ impl CargoSaftCmd {
     fn build_command(self) -> Command {
         let mut cmd = Command::new("cargo");
 
-        cmd.env("RUSTC_WRAPPER", Self::analyzer_path())
+        cmd.env("RUSTC_WRAPPER", Self::driver_path())
             .arg(self.cargo_subcmd)
             .args(self.args);
+
+        
+        // Set the tool chain to be compatible with saft
+        if let Some(toolchain) = option_env!("RUSTUP_TOOLCHAIN") {
+            cmd.env("RUSTUP_TOOLCHAIN", toolchain);
+
+            let mut wasm32_toolchain = Command::new(OsString::from("rustup"));
+            wasm32_toolchain
+                .arg("target")
+                .arg("add")
+                .arg("wasm32-unknown-unknown")
+                .arg("--toolchain")
+                .arg(toolchain);
+            wasm32_toolchain.spawn().expect("failed to set target");
+        }
+
+        cmd
+    }
+
+    fn command_always_encode_mir() -> Command {
+        let mut cmd = Command::new("cargo");
+        cmd.arg("check").arg("--lib");
+        cmd.arg("--release");
+        cmd.env("RUSTFLAGS", "--cfg saft -Z always_encode_mir");
 
         cmd
     }
@@ -61,7 +82,8 @@ fn process<I>(old_args: I) -> Result<(), i32>
 where
     I: Iterator<Item = String>,
 {
-    let mut cmd = CargoSaftCmd::new(old_args).build_command();
+    // Recompile with the wanted config
+    let mut cmd = CargoSaftCmd::command_always_encode_mir();
 
     let exit_status = cmd
         .spawn()
@@ -70,7 +92,20 @@ where
         .expect("failed to wait for cargo");
 
     if exit_status.success() {
-        Ok(())
+        // Delegate analysis to driver
+        let mut cmd = CargoSaftCmd::new(old_args).build_command();
+
+        let exit_status = cmd
+            .spawn()
+            .expect("could not run cargo")
+            .wait()
+            .expect("failed to wait for cargo");
+
+        if exit_status.success() {
+            Ok(())
+        } else {
+            Err(exit_status.code().unwrap_or(-1))
+        }
     } else {
         Err(exit_status.code().unwrap_or(-1))
     }
