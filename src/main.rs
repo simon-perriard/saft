3e8c4f797a962d5b61f21cfd9b06f04180e66435
Simon Perriard <simon.perriard@epfl.ch>
2022-04-08 14:42:46 +0200

    simple folder layout

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,29 @@
-#![feature(let_chains)]
-#![feature(exit_status_error)]
+use std::env;
+use std::process::Command;
 
-use std::{
-    env,
-    ffi::OsStr,
-    path::{Path, PathBuf},
-    process::{self, Command},
-};
+// Unfortunately, we cannot run the analysis directly on a given crate of interest. The reason is
+// that the given crate may depend on other crates, and they must be passed as arguments to `rustc`.
+// However, `cargo` does not offer a simple way to obtain these arguments.  That is why, we employ a
+// standard trick, running the analysis as part of a `cargo check`.  We instruct `cargo` to use our
+// driver for checking crates in the workspace.  For each check, `cargo` invokes the driver with the
+// required arguments.  Then, the driver runs `rustc` and hooks at convenient stages to perform the
+// analysis.  After the analysis is finished, the driver lets `rustc` emit the metadata it computed
+// about the current crate, because this metadata may be needed for checking other crates.
+fn main() {
+    // Obtain driver from the current executable.
+    let driver_path = env::current_exe()
+        .expect("invalid current executable path")
+        .with_file_name("saft-driver");
 
-pub fn main() {
-    match env::args().nth(1).as_ref().map(AsRef::as_ref) {
-        Some("saft") => {
-            recompile_with_mir();
-        }
-        Some("rustc") => {
-            dispatch_rustc_call();
-        }
-        _ => unreachable!(),
-    }
-}
-
-fn get_val_from_cli_flag(name: &str) -> Option<String> {
-    let mut args = env::args();
-    while let Some(arg) = args.next() {
-        if arg.starts_with(name) {
-            if arg.len() == name.len() {
-                return args.next();
-            } else if let Some(val) = arg.split('=').nth(1) {
-                return Some(val.to_string());
-            } else {
-                return None;
-            }
-        }
-    }
-    None
-}
-
-fn get_manifest_metadata() -> cargo_metadata::Metadata {
-    let manifest_path = get_val_from_cli_flag("--manifest-path")
-        .map(|manifest_path| Path::new(&manifest_path).canonicalize().unwrap());
-
-    let mut cmd = cargo_metadata::MetadataCommand::new();
-    if let Some(manifest_path) = manifest_path {
-        cmd.manifest_path(manifest_path);
-    }
-
-    cmd.exec().expect("Cannot query manifest metadata")
-}
-
-fn command_call_procedure(mut cmd: Command) {
-    let exit_status = cmd
+    // Run the analysis as part of a `cargo check`.
+    Command::new("cargo")
+        .env("RUSTC_WORKSPACE_WRAPPER", driver_path) // analyze only crates in the workspace
+        .env("RUSTCFLAGS", "--cfg saft -Z always-encode-mir") // emit mir for all dependencies
+        .arg("check")
+        .arg("--lib")
+        .args(env::args().skip(2))
         .spawn()
         .expect("could not run cargo")
         .wait()
         .expect("failed to wait for cargo");
-
-    if let Err(code) = exit_status.exit_ok() {
-        process::exit(code.code().unwrap_or(-1));
-    }
-}
-
-fn recompile_with_mir() {
-    let metadata = get_manifest_metadata();
-    let root = metadata.root_package().unwrap();
-
-    // Pallets only have one target, and it is a lib
-    let target = &root.targets[0];
-
-    let mut cmd = Command::new("cargo");
-    cmd.arg("check").arg("--lib");
-
-    let cli_args = env::args().skip(2);
-
-    // Add cargo flags
-    for arg in cli_args {
-        if arg.as_str() == "--" {
-            // Every remaining args after -- are saft args
-            break;
-        }
-        cmd.arg(arg);
-    }
-
-    // Force recompile to encode MIR for every dependencies
-    cmd.env("RUSTFLAGS", "--cfg saft -Z always_encode_mir");
-
-    // Use our `main` as a wrapper for rustc, where calls will be dispatched.
-    // It will act as a recursive call to `main`, but control flow will go to `rustc` branch.
-    cmd.env(
-        "RUSTC_WRAPPER",
-        env::current_exe().expect("Current executable path is invalid."),
-    );
-
-    // Set the target name for the recursive call for correct dispatch
-    cmd.env("SAFT_TARGET", target.name.replace('-', "_"));
-
-    command_call_procedure(cmd);
-}
-
-fn get_driver_path() -> PathBuf {
-    let mut path = env::current_exe()
-        .expect("Current executable path is invalid.")
-        .with_file_name("saft-driver");
-
-    if cfg!(windows) {
-        path.set_extension("exe");
-    }
-
-    path
-}
-
-fn invoke_program_with_same_calling_args<S>(program: S)
-where
-    S: AsRef<OsStr>,
-{
-    let mut cmd = Command::new(program);
-
-    // Copy all the cli args from calling procedure
-    cmd.args(env::args().skip(2));
-
-    command_call_procedure(cmd);
-}
-
-fn dispatch_rustc_call() {
-    // If compiling the target crate,
-    // give control to our driver for analysis.
-    // Otherwise just compile.
-
-    if let Some(crate_name) = get_val_from_cli_flag("--crate-name")
-    && let Ok(target) = env::var("SAFT_TARGET")
-    && target.eq(&crate_name)
-    {
-        invoke_program_with_same_calling_args(get_driver_path());
-    } else {
-        invoke_program_with_same_calling_args("rustc");
-    }
 }
