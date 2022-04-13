a29336ab241c7f71aedbf3b9eecd37bc6c6de911
Simon Perriard <simon.perriard@epfl.ch>
2022-04-13 18:22:46 +0200

    reconstruct called type from FnDef

--- src/lib.rs
+++ src/lib.rs
@@ -10,26 +10,26 @@ extern crate rustc_interface;
 extern crate rustc_middle;
 extern crate rustc_span;
 extern crate rustc_typeck;
+extern crate rustc_expand;
 
 pub mod analysis;
 pub mod sysroot;
-
 use crate::analysis::*;
 
 pub fn extract_juice<'tcx>(tcx: rustc_middle::ty::TyCtxt<'tcx>) {
     // Extract pallet
-    print!("Extracting pallet information...");
+    //print!("Extracting pallet information...");
     let pallet = pallet::Pallet::new(tcx);
-    println!(" Done");
+    //println!(" Done");
 
-    println!("The following dispatchables will be analyzed :");
-    analysis_utils::dispatchables_getter::print_dispatchable_names(tcx, &pallet.dispatchables);
+    //println!("The following dispatchables will be analyzed :");
+    //analysis_utils::dispatchables_getter::print_dispatchable_names(tcx, &pallet.dispatchables);
 
     for dispatchables_def_id in pallet.dispatchables.keys() {
         let mut dispatchable_visitor =
             dispatchable_visitor::DispatchableVisitor::new(tcx, &pallet, *dispatchables_def_id);
-        print!("Analyzing {}...", dispatchable_visitor.get_fn_name());
+        //print!("Analyzing {}...", dispatchable_visitor.get_fn_name());
         dispatchable_visitor.visit_body();
-        println!(" Done")
+        //println!(" Done")
     }
 }
